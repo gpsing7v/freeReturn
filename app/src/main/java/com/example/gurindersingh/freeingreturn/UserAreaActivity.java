e3a1df05bac0841220dadf7ef90f4c2da3aa5c84
Gurinder Singh <gsingh1@uga.edu>
2016-07-07 12:11:25 -0400

    first commit

--- app/src/main/java/com/example/gurindersingh/freeingreturn/UserAreaActivity.java
+++ app/src/main/java/com/example/gurindersingh/freeingreturn/UserAreaActivity.java
@@ -8,20 +8,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.Toast;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView.OnItemSelectedListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import java.util.Calendar;
 
-public class UserAreaActivity extends AppCompatActivity {
+public class UserAreaActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     Button date;
     DatePickerDialog datePickerDialog;
     ActionBarDrawerToggle toggle;
+    Spinner spinner;
+    Button search;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -54,9 +64,48 @@ public class UserAreaActivity extends AppCompatActivity {
             }
         });
 
+        spinner= (Spinner) findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+
+        List<String> retailers = new ArrayList<String>();
+        retailers.add("Kroger");
+        retailers.add("Costco");
+        retailers.add("The Home Depot");
+        retailers.add("Walgreens");
+        retailers.add("Target");
+        retailers.add("CVS");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, retailers);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+
+//        ArrayAdapter adapter=ArrayAdapter.createFromResource(this,R.array.Retailer,android.R.layout.simple_spinner_item);
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+//        spinner.setAdapter(adapter);
+//        spinner.setOnItemSelectedListener(this);
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
 
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+//        TextView myText= (TextView) view;
+//        Toast.makeText(this, "You Selected"+myText.getText(), Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
